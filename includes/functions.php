9f7576c7a3dd32c4935a39bc8007661e8a65e2c9
Stefan Muhlack <stefan.muhlack@wwe-media.eu>
2023-10-24 16:14:42 +0200

    init 2

--- includes/functions.php
+++ includes/functions.php
@@ -5,14 +5,14 @@ include 'db.php';
 function getTemplates() {
     global $pdo;
     $stmt = $pdo->query('SELECT * FROM templates');
-    return $stmt->fetchAll();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 function getTemplateById($id) {
     global $pdo;
     $stmt = $pdo->prepare('SELECT * FROM templates WHERE id = ?');
     $stmt->execute([$id]);
-    return $stmt->fetch();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
 // Functions for pages
@@ -20,14 +20,14 @@ function getPagesByTemplateId($templateId) {
     global $pdo;
     $stmt = $pdo->prepare('SELECT * FROM pages WHERE template_id = ?');
     $stmt->execute([$templateId]);
-    return $stmt->fetchAll();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 function getPageById($id) {
     global $pdo;
     $stmt = $pdo->prepare('SELECT * FROM pages WHERE id = ?');
     $stmt->execute([$id]);
-    return $stmt->fetch();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
 // Functions for rectangles
@@ -35,14 +35,14 @@ function getRectanglesByPageId($pageId) {
     global $pdo;
     $stmt = $pdo->prepare('SELECT * FROM rectangles WHERE page_id = ?');
     $stmt->execute([$pageId]);
-    return $stmt->fetchAll();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 function getRectangleById($id) {
     global $pdo;
     $stmt = $pdo->prepare('SELECT * FROM rectangles WHERE id = ?');
     $stmt->execute([$id]);
-    return $stmt->fetch();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
 // Functions for detected values
@@ -50,14 +50,37 @@ function getDetectedValuesByRectangleId($rectangleId) {
     global $pdo;
     $stmt = $pdo->prepare('SELECT * FROM detected_values WHERE rectangle_id = ?');
     $stmt->execute([$rectangleId]);
-    return $stmt->fetchAll();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 function getDetectedValueById($id) {
     global $pdo;
     $stmt = $pdo->prepare('SELECT * FROM detected_values WHERE id = ?');
     $stmt->execute([$id]);
-    return $stmt->fetch();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+// Additional functions for user management
+function getUserById($id) {
+    global $pdo;
+    $stmt = $pdo->prepare('SELECT * FROM users WHERE id = ?');
+    $stmt->execute([$id]);
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+function getUserByUsername($username) {
+    global $pdo;
+    $stmt = $pdo->prepare('SELECT * FROM users WHERE username = ?');
+    $stmt->execute([$username]);
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+function userHasRole($userId, $roleName) {
+    global $pdo;
+    $stmt = $pdo->prepare('SELECT COUNT(*) as count FROM user_roles JOIN roles ON user_roles.role_id = roles.id WHERE user_roles.user_id = ? AND roles.role_name = ?');
+    $stmt->execute([$userId, $roleName]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result['count'] > 0;
 }
 
 ?>
